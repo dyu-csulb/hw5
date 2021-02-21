2c4bd4675efa7778e74603e7586cb694bcbf5697
David Yu <david.yu@outlook.com>
2021-02-20 16:48:36 -0800

    Completed exercise 1

--- js/ex1.js
+++ js/ex1.js
@@ -1,61 +1,34 @@
+
+class Square {
+    constructor(v_side) {
+      this.side = v_side;
+    }
+
+    describe() {
+        let message = "";
+
+        this.perimeter = Number( this.side) * 4;
+        this.area = Number(Math.pow( this.side,2));
+        this.diagonal = Number(Math.sqrt(2) * Number( this.side)).toFixed(3);
+
+        return message = `Square with a side of ${this.side} has a perimeter of ${this.perimeter}, and area of ${this.area}, and diagonal of ${this.diagonal}`
+    }
+  }
+
+
 function myFunction() {
-    let num1 = 0;
-    let num2 = 0;
-    let operator = '';
-
-    num1 = document.getElementById("txtNum1").value;
-    num2 = document.getElementById("txtNum2").value;
-    operator = document.getElementById("operator").value;
-
-    if (num1=='') {
-        alert('Please enter a valid first number!')
-        document.getElementById("txtNum1").value  = '';
-        document.getElementById("txtNum1").focus();
-    }
-    else if (num2=='') {
-        alert('Please enter a valid second number!')
-        document.getElementById("txtNum2").value  = '';
-        document.getElementById("txtNum2").focus();
-    }
-    else 
-    {
-        calculateValues(num1, num2, operator);
-    }
+    let val = document.getElementById("txtNum1").value;
+    let obj = new Square(val);
+    let returnVal = obj.describe();
+    
+    console.log(returnVal);
+    document.getElementById("lblMessage").innerHTML  = returnVal;
 }
 
-function calculateValues(num1, num2, operator) {
-    let total = 0;
-    let answer = '';
 
-    if (operator==1) {
-        //add
-        total = Number(num1) + Number(num2)  
-        answer = num1 + ' + '  + num2 + ' = ' + total 
-    } 
-    else if (operator==2) {
-        //subtract
-        total = Number(num1) - Number(num2)  
-        answer = num1 + ' - '  + num2 + ' = ' + total 
-    }
-    else if (operator==3) {
-        //multiply
-        total = Number(num1) * Number(num2)  
-        answer = num1 + ' * '  + num2 + ' = ' + total 
-    }
-    else if (operator==4) {
-        //divide
-        total = Number(num1) / Number(num2)  
-        answer = num1 + ' / '  + num2 + ' = ' + total 
-    }
-
-    document.getElementById("txtAnswer").value  =  answer;
-    console.log(answer);
-}
 
 function clearValues() {
     document.getElementById("txtNum1").value  = '';
-    document.getElementById("txtNum2").value  = '';
     document.getElementById("lblMessage").innerHTML  = '';
-    document.getElementById("txtAnswer").value  =  '';
     document.getElementById("txtNum1").focus();
 }
