8526cedbbbb1bdabc546201df62cd28a2e34d6d5
David Yu <david.yu@outlook.com>
2021-02-21 15:11:14 -0800

    updated again.

--- js/ex1.js
+++ js/ex1.js
@@ -11,7 +11,7 @@ class Square {
         this.area = Number(Math.pow( this.side,2));
         this.diagonal = Number(Math.sqrt(2) * Number( this.side)).toFixed(3);
 
-        return message = `Square with a side of ${this.side} has a perimeter of ${this.perimeter}, and area of ${this.area}, and diagonal of ${this.diagonal}`
+        return message = `Square with a side of ${this.side} has a perimeter of ${this.perimeter}, an area of ${this.area}, and diagonal of ${this.diagonal}`
     }
   }
 
