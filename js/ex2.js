d17cb52a3b1913e5d89e96fe1fa5afc4bf059aeb
David Yu <david.yu@outlook.com>
2021-02-21 15:07:41 -0800

    Completed exercise 2

--- js/ex2.js
+++ js/ex2.js
@@ -2,40 +2,34 @@
 function myFunction() {
     let myArray = []
     let message = '';
+    
     // Load array with values 1 to 10
     for (i = 1; i < 11; i++) {
         myArray.push(i);
     }
 
-    let odd = myArray.filter(number => number%2);
+    let a = myArray.filter(number => number%2);
+    let b = myArray.filter(number =>!(number%2 || number%5))
+    let c = myArray.filter(number =>!(number%3)).map(x => x ** 2)
+    let d = myArray.filter(number =>!(number%5)).reduce((a, b) => a + b) 
+    let d2 = d ** 2
 
     console.log(myArray);
-    console.log(odd);
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d2);
 
+    message = 'a. Odd array values:  ' + a + '<br/>' 
+            + 'b. Array values divisible by 2 or 5 is: ' + b + '<br/>' 
+            + 'c. Array of numbers divisible by 3 squared: ' + c + '<br/>' 
+            + 'd. Square the numbers divisible by 5 is: ' + d2 + '<br/>' 
 
-    
-    message ='Array values: ' +  myArray + '<br/>' + 'Odd array values:  ' + odd;
     document.getElementById("lblMessage").innerHTML  =  message;
 }
 
 
-function myFunction1() {
-    let v_radius =   document.getElementById("txtNum1").value 
-    let answer = '';
-    const circle = {
-            circumference: "50",
-            area: "20",
-            radius:  v_radius
-    }
 
-    console.log(circle.circumference)
-    console.log(circle.area)
-    console.log(circle.radius)
-
-    answer = 'Its circumference is: '+ circle.circumference + '<br/>' + 'Its area is: ' + circle.area + '<br/>Its radius is: ' + circle.radius
-    document.getElementById("lblMessage").innerHTML  =  answer;
-    console.log(answer.replaceAll("<br/>", "\n"));
-}
 
 function clearValues() {
     document.getElementById("lblMessage").innerHTML  = '';
